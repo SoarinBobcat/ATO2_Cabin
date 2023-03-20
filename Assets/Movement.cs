95c8a92796b1ccce7f3c79da460aeca74393807a
SoarinBobcat <125240893+SoarinBobcat@users.noreply.github.com>
2023-03-20 15:59:39 +0800

    Movement Started

---
+++ Assets/Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int spd = 8;
+        Vector3 currSpd = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
