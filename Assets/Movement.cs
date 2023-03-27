6080bced17e245128aac86fc057b343542cbeb44
SoarinBobcat <125240893+SoarinBobcat@users.noreply.github.com>
2023-03-27 15:48:56 +0800

    Movement Completed

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    int spd = 4;
+    Vector3 currSpd = Vector3.zero;
+
+    float x_move = 0f;
+    float z_move = 0f;
+
+    public CharacterController Capsule;
+
     // Start is called before the first frame update
     void Start()
     {
-        int spd = 8;
-        Vector3 currSpd = Vector3.zero;
+        Capsule = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        x_move = Input.GetAxis("Horizontal")*spd*Time.deltaTime;
+        z_move = Input.GetAxis("Vertical")*spd*Time.deltaTime;
+
+ 
+        currSpd = (transform.forward * z_move) + (transform.right *x_move);
         
+
+
+        Capsule.Move(currSpd);
     }
 }
